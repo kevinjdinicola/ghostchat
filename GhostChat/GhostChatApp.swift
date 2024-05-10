1c4de020f3d0bccb8cb93811364443d28130f67b
kevin <kevinjdinicola@gmail.com>
2024-05-10 09:03:21 -0700

    Initial Commit

---
+++ GhostChat/GhostChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhostChatApp.swift
+//  GhostChat
+//
+//  Created by Kevin Dinicola on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GhostChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
